72f6d55efc7e410580281c574c3694209779c998
ebuka <sebuka559@gmail.com>
2024-11-09 15:07:30 +0100

    final commit

--- file_manager/models.py
+++ file_manager/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')  # Directory where the file will be stored
-    uploaded_at = models.DateTimeField(auto_now_add=True)  # Automatically sets the current timestamp when file is uploaded
-    file_size = models.BigIntegerField(null=True, blank=True)  # Stores the file size in bytes
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)  
+    file_size = models.BigIntegerField(null=True, blank=True)  
     
     def save(self, *args, **kwargs):
-        # Calculate and store the file size before saving the model
+        
         if self.file:
             self.file_size = self.file.size
         super(UploadedFile, self).save(*args, **kwargs)
